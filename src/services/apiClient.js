25b3d196915bab9b2b9dfd382ec15e0419d3886a
gsaru <gsarun7@gmail.com>
2025-12-28 15:10:20 +0530

    fixed submit request for sale return, purchase, invoice

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://ngtbackend-e4ab.onrender.com",
   headers: {
     "Content-Type": "application/json",
     "Cache-Control": "no-cache"
